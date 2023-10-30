83ffeec797d89a478d3cd34976e62f2a13162a73
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-10-30 13:51:31 +0630

    add document comment

--- lib/native_id.dart
+++ lib/native_id.dart
@@ -1,5 +1,6 @@
 import 'native_id_platform_interface.dart';
 
+/// [NativeId] plugin to get `unique identifier` on `Android` and `iOS`
 class NativeId {
   /// Get unique identifier.
   Future<String?> getId() {
